0728db8a651d72752c11f78f8d1ae8f8af93bba5
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2014-12-04 15:41:42 +0900

    Set default http header via overriding http methods

--- lib/rspec/default_http_header.rb
+++ lib/rspec/default_http_header.rb
@@ -1,7 +1,19 @@
 require "rspec/default_http_header/version"
 
-module Rspec
+module RSpec
   module DefaultHttpHeader
-    # Your code goes here...
+    extend ActiveSupport::Concern
+
+    HTTP_METHODS = %w(get post put delete patch)
+
+    included do
+      let(:default_headers) { {} }
+
+      HTTP_METHODS.each do |m|
+        define_method(m) do |path, parameters = nil, headers_or_env = nil|
+          super(path, parameters, default_headers.merge(headers_or_env))
+        end
+      end
+    end
   end
 end
